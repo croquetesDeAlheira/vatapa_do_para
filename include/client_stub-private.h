57fb3fd5c226e598de7eac417141a4ded549e645
lmbcosta <lmbcosta@users.noreply.github.com>
2016-11-07 21:09:41 +0000

    Update client_stub-private.h
    merging client stub private h

--- include/client_stub-private.h
+++ include/client_stub-private.h
@@ -1,11 +1,13 @@
-#ifndef _CLIENT_STUB-PRIVATE_H
-#define _CLIENT_STUB-PRIVATE_H
+#ifndef _CLIENT_STUB_PRIVATE_H
+#define _CLIENT_STUB_PRIVATE_H
+
+#include "client_stub.h"
 
 /* Remote table. A definir pelo grupo em client_stub-private.h 
  */
 struct rtable_t{
-	struct server_t server;
-}
+	struct server_t *server;
+};
 
 
 #endif
